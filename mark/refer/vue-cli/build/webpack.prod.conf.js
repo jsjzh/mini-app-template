8badc7f4dba5ad82ef1ac03437b1b274b9ec4b73
jsjzh <kimimi_king@163.com>
2018-07-20 15:03:17 +0800

    add project refer

--- mark/refer/vue-cli/build/webpack.prod.conf.js
+++ mark/refer/vue-cli/build/webpack.prod.conf.js
@@ -53,16 +53,14 @@ const webpackConfig = merge(baseWebpackConfig, {
     // Compress extracted CSS. We are using this plugin so that possible
     // duplicated CSS from different components can be deduped.
     new OptimizeCSSPlugin({
-      cssProcessorOptions: config.build.productionSourceMap ?
-        {
-          safe: true,
-          map: {
-            inline: false
-          }
-        } :
-        {
-          safe: true
+      cssProcessorOptions: config.build.productionSourceMap ? {
+        safe: true,
+        map: {
+          inline: false
         }
+      } : {
+        safe: true
+      }
     }),
     // generate dist index.html with correct asset hash for caching.
     // you can customize output by editing /index.html
