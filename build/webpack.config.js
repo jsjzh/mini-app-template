ae7535b4b19304298d1605dd46b6e3e05b637bc5
jsjzh <kimimi_king@163.com>
2018-08-21 22:25:59 +0800

    i wan't sleep !

--- build/webpack.config.js
+++ build/webpack.config.js
@@ -2,8 +2,8 @@ const path = require("path");
 const os = require("os");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const webpack = require("webpack");
-const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin")
-const packageConfig = require('../package.json')
+const FriendlyErrorsPlugin = require("friendly-errors-webpack-plugin");
+const packageConfig = require('../package.json');
 
 function resolve(dir) {
   return path.join(__dirname, "..", dir)
@@ -37,19 +37,24 @@ function createNotifierCallback() {
   }
 }
 
+function assetsPath(_path) {
+  return path.posix.join("static", _path)
+}
+
 const config = {
   context: resolve("./"),
   mode: "development",
-  devtool: "cheap-module-eval-source-map",
   entry: {
     app: ["babel-polyfill", "./src/index.js"]
   },
+  devtool: "cheap-module-eval-source-map",
   output: {
     path: resolve("dist"),
-    filename: "[name].js"
+    filename: "[name].js",
+    publicPath: "/"
   },
   resolve: {
-    extensions: ['.js', '.vue', '.json'],
+    extensions: ['.js', '.json'],
     alias: {
       '@': resolve('src'),
       'static': resolve('static')
@@ -76,7 +81,7 @@ const config = {
         loader: "url-loader",
         options: {
           limit: 10000,
-          name: path.posix.join("static", "img/[name].[hash:7].[ext]")
+          name: assetsPath("img/[name].[hash:7].[ext]")
         }
       },
       {
@@ -84,7 +89,7 @@ const config = {
         loader: "url-loader",
         options: {
           limit: 10000,
-          name: path.posix.join("static", "media/[name].[hash:7].[ext]")
+          name: assetsPath("media/[name].[hash:7].[ext]")
         }
       },
       {
@@ -92,7 +97,7 @@ const config = {
         loader: "url-loader",
         options: {
           limit: 10000,
-          name: path.posix.join("static", "fonts/[name].[hash:7].[ext]")
+          name: assetsPath("fonts/[name].[hash:7].[ext]")
         }
       }
     ]
@@ -108,13 +113,8 @@ const config = {
       errors: true
     },
     quiet: true,
-    watch: true,
-    watchOptions: {
-      ignored: /node_modules/
-    },
     host: getIPAdress(),
-    publicPath: resolve("/"),
-    proxy: {},
+    publicPath: "/"
   },
   plugins: [
     new webpack.DefinePlugin({
@@ -136,6 +136,4 @@ const config = {
   ]
 }
 
-module.exports = new Promise((resolve, reject) => {
-  resolve(config)
-})
+module.exports = config;
