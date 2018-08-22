6b35032d8afb9935b8ad4c61b7d190d40b1b519d
jsjzh <kimimi_king@163.com>
2018-08-22 14:17:01 +0800

    fix

--- build/utils.js
+++ build/utils.js
@@ -1,39 +1,39 @@
 const path = require("path");
 const os = require("os");
-const packageConfig = require('../package.json');
+const packageConfig = require("../package.json");
 
 exports.resolve = function (dir) {
-    return path.join(__dirname, "..", dir)
+  return path.join(__dirname, "..", dir)
 }
 
 exports.getIPAdress = function () {
-    var interfaces = os.networkInterfaces();
-    for (var devName in interfaces) {
-        var iface = interfaces[devName];
-        for (var i = 0; i < iface.length; i++) {
-            var alias = iface[i];
-            if (alias.family === "IPv4" && alias.address !== "127.0.0.1" && !alias.internal) {
-                return alias.address;
-            }
-        }
+  const interfaces = os.networkInterfaces();
+  for (const devName in interfaces) {
+    const iface = interfaces[devName];
+    for (let i = 0; i < iface.length; i++) {
+      const alias = iface[i];
+      if (alias.family === 'IPv4' && alias.address !== '127.0.0.1' && !alias.internal) {
+        return alias.address;
+      }
     }
+  }
 }
 
 exports.createNotifierCallback = function () {
-    var notifier = require('node-notifier')
-    return (severity, errors) => {
-        if (severity !== 'error') return
-        var error = errors[0]
-        var filename = error.file && error.file.split('!').pop()
+  const notifier = require("node-notifier")
+  return (severity, errors) => {
+    if (severity !== "error") return
+    const error = errors[0]
+    const filename = error.file && error.file.split("!").pop()
 
-        notifier.notify({
-            title: packageConfig.name,
-            message: severity + ': ' + error.name,
-            subtitle: filename || ''
-        })
-    }
+    notifier.notify({
+      title: packageConfig.name,
+      message: severity + ": " + error.name,
+      subtitle: filename || ""
+    })
+  }
 }
 
 exports.assetsPath = function (_path) {
-    return path.posix.join("static", _path)
+  return path.posix.join("static", _path)
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
+import "static/css/normalize.css";
+
 import * as index from "./index.html";
 import "./index.css";
 
 console.log(index);
-
 
 const app = document.getElementById("app");
 app.innerHTML = index;
